a8c8e688c6aed504953931a0ba2ef9bd8a213707
tmorgan1987 <tmorgan1987@outlook.com>
2022-10-16 14:20:53 -0400

    questions

--- index.js
+++ index.js
@@ -3,7 +3,47 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		type: 'input',
+		name: 'title',
+		message: 'What is the title of the project you would like to create a Readme file for?',
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: 'Please describe your project.',
+	},
+	{
+		type: 'input',
+		name: 'installation',
+		message: 'How do you install/use your project?',
+	},
+	{
+		type: 'input',
+		name: 'contributions',
+		message: 'Whom/what contributed to the completion of your project besides yourself, if any?'
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: 'What is a good email address to contact you should users have questions about your project?',
+	},
+	{
+		type: 'input',
+		name: 'github',
+		message: 'Where can users contact you on github?',
+	},
+	{
+		type: 'list',
+		name: 'license',
+		message: 'What licenses did you use?',
+		choices: ['MIT', 'ISC', 'GNUPLv3'],
+		filter(val) {
+			return val.toLowerCase();
+		}
+	}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
